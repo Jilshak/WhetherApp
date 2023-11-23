0f84edd7d6ba4f178a5f404eb0b4fec7228cc088
Jilshak <mohd.jilshak@gmail.com>
2023-11-23 11:20:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react"
+import axios from 'axios'
+import weather_image from './images/weather.jpg'
+
+
+function App() {
+
+  const [serach, setSearch] = useState('')
+  const [data, setData] = useState(null)
+
+  const handleSerach = async () => {
+    const wether = await axios.get(`${api.baseUrl}weather?q=${serach}&units=metric&&APPID=${api.key}`)
+    if (wether.status == 200) {
+      console.log(wether.data)
+      setData(wether.data)
+    } else {
+      alert("Something is wrong with the network please try again after some time")
+    }
+  }
+
+  const api = {
+    key: '095af7b62ee6725f2753d671c9713500',
+    baseUrl: 'https://api.openweathermap.org/data/2.5/'
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-center z-30 h-[100vh]">
+        <div className="mockup-phone border-primary z-30">
+          <div className="camera"></div>
+          <div className="display relative">
+            <img className="h-[550px] w-[300px] rounded-lg relative z-10" src={weather_image} alt="" />
+            <div>
+              <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Enter Location" className="input border-0 mx-10 opacity-50 bg-[#e4e3e3] input-sm input-bordered input-info w-full absolute top-10 z-20 max-w-[230px]" />
+              <button onClick={handleSerach} className="btn btn-xs absolute top-[44px] right-[40px] w-[40px] text-[8px] z-20 btn-outline">
+                search
+              </button>
+            </div>
+            <div className="z-50 absolute top-32 ">
+              <p className="font-extrabold text-2xl text-white mx-5">{data?.name ? data?.name : "London"}</p>
+              <p className="font-extrabold text-5xl text-white mx-5">{data?.main?.temp ? data?.main?.temp : '60'}°F</p>
+              <p className="font-extrabold text-xl text-white mx-5">{data?.weather[0]?.main ? data?.weather[0]?.main : 'Clouds'}</p>
+            </div>
+            <div>
+              <div className="join absolute z-50 bottom-7 right-2  opacity-30 bg-[#e4e3e3]">
+                <button className="btn join-item z-30">
+                  <div className="opacity-100">
+                    <div>{data?.main?.feels_like ? data?.main?.feels_like : '60'}°F</div>
+                    <div>Feels like</div>
+                  </div>
+                </button>
+                <button className="btn join-item z-30">
+                  <div className="opacity-100">
+                    <div>{data?.main?.humidity ? data?.main?.humidity : 0}%</div>
+                    <div>Humidity</div>
+                  </div>
+                </button>
+                <button className="btn join-item z-30">
+                  <div className="opacity-100">
+                    <div>{data?.wind?.speed ? data?.wind?.speed : 12 }MPH</div>
+                    <div>Wind Speed</div>
+                  </div>
+                </button>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/images/out.jsx
@@ -0,0 +1,43 @@
+text = {
+    "coord": {
+        "lon": -0.1257,
+        "lat": 51.5085
+    },
+    "weather": [
+        {
+            "id": 802,
+            "main": "Clouds",
+            "description": "scattered clouds",
+            "icon": "03n"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 9.65,
+        "feels_like": 7.1,
+        "temp_min": 7.83,
+        "temp_max": 12.11,
+        "pressure": 1026,
+        "humidity": 92
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 5.14,
+        "deg": 270
+    },
+    "clouds": {
+        "all": 40
+    },
+    "dt": 1700709893,
+    "sys": {
+        "type": 2,
+        "id": 2075535,
+        "country": "GB",
+        "sunrise": 1700724679,
+        "sunset": 1700755357
+    },
+    "timezone": 0,
+    "id": 2643743,
+    "name": "London",
+    "cod": 200
+}
